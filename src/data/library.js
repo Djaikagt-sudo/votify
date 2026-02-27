801dd8701b96fb6eb66523380c46ba0118fe2800
Charly Garcia <charlygarcia@MacBook-Pro-de-Charly.local>
2026-02-26 19:28:46 -0600

    Update library

--- src/data/library.js
+++ src/data/library.js
@@ -1,63 +1,99 @@
 export const library = {
 
-  pop: [
-    { artist: "Ed Sheeran", title: "Shape of You", youtubeId: "" },
-    { artist: "Dua Lipa", title: "Levitating", youtubeId: "" },
-    { artist: "The Weeknd", title: "Blinding Lights", youtubeId: "" }
-  ],
-
-  banda: [
-    { artist: "Banda MS", title: "El Color de Tus Ojos", youtubeId: "" },
-    { artist: "Julion Alvarez", title: "Te Hubieras Ido Antes", youtubeId: "" },
-    { artist: "Christian Nodal", title: "Adios Amor", youtubeId: "" }
-  ],
-
-  corridos: [
-    { artist: "Peso Pluma", title: "Ella Baila Sola", youtubeId: "" },
-    { artist: "Natanael Cano", title: "Amor Tumbado", youtubeId: "" },
-    { artist: "Fuerza Regida", title: "TQM", youtubeId: "" }
-  ],
-
-  salsa: [
-    { artist: "Marc Anthony", title: "Vivir Mi Vida", youtubeId: "" },
-    { artist: "Hector Lavoe", title: "El Cantante", youtubeId: "" },
-    { artist: "Grupo Niche", title: "Cali Pachanguero", youtubeId: "" }
-  ],
-
-  cumbia: [
-    { artist: "Los Angeles Azules", title: "Como Te Voy A Olvidar", youtubeId: "" },
-    { artist: "Sonora Dinamita", title: "Mi Cucu", youtubeId: "" },
-    { artist: "Grupo Frontera", title: "No Se Va", youtubeId: "" }
-  ],
-
-  merengue: [
-    { artist: "Juan Luis Guerra", title: "La Bilirrubina", youtubeId: "" },
-    { artist: "Elvis Crespo", title: "Suavemente", youtubeId: "" },
-    { artist: "Wilfrido Vargas", title: "El Africano", youtubeId: "" }
-  ],
-
-  rockEnEspanol: [
-    { artist: "Soda Stereo", title: "De Musica Ligera", youtubeId: "" },
-    { artist: "Mana", title: "Clavado En Un Bar", youtubeId: "" },
-    { artist: "Heroes del Silencio", title: "Entre Dos Tierras", youtubeId: "" }
-  ],
-
-  rockGuatemalteco: [
-    { artist: "Malacates Trebol Shop", title: "Morena", youtubeId: "" },
-    { artist: "Bohemia Suburbana", title: "Peces e Iguanas", youtubeId: "" },
-    { artist: "Alux Nahual", title: "Como Un Duende", youtubeId: "" }
-  ],
-
-  reggaetonAntiguo: [
-    { artist: "Daddy Yankee", title: "Gasolina", youtubeId: "" },
-    { artist: "Don Omar", title: "Danza Kuduro", youtubeId: "" },
-    { artist: "Wisin y Yandel", title: "Rakata", youtubeId: "" }
-  ],
-
+  // =========================
+  // REGGAETON NUEVO
+  // =========================
   reggaetonNuevo: [
-    { artist: "Bad Bunny", title: "Monaco", youtubeId: "" },
-    { artist: "Feid", title: "Classy 101", youtubeId: "" },
-    { artist: "Karol G", title: "Provenza", youtubeId: "" }
+    { artist:"Bad Bunny", title:"MONACO", youtubeId:"_S3UjMZC0Tk" },
+    { artist:"Feid", title:"Classy 101", youtubeId:"lZiaYpD9ZrI" },
+    { artist:"Karol G", title:"Provenza", youtubeId:"F0B7HDiY-10" },
+    { artist:"Myke Towers", title:"LALA", youtubeId:"_tFz3rFfS3M" },
+    { artist:"Rauw Alejandro", title:"Todo De Ti", youtubeId:"CFPLIaMpGrY" },
+    { artist:"Anuel AA", title:"China", youtubeId:"0VR3dfZf9Yg" },
+    { artist:"Ozuna", title:"Caramelo", youtubeId:"q3xHk8i4e8o" },
+    { artist:"J Balvin", title:"Rojo", youtubeId:"1DpH-icPpl0" },
+    { artist:"Quevedo", title:"Columbia", youtubeId:"lZiaYpD9ZrI" },
+    { artist:"Yandel", title:"Yandel 150", youtubeId:"Z9eMk051" }
+  ],
+
+  // =========================
+  // REGGAETON ANTIGUO
+  // =========================
+  reggaetonAntiguo: [
+    { artist:"Daddy Yankee", title:"Gasolina", youtubeId:"3tw2P65wv5E" },
+    { artist:"Don Omar", title:"Danza Kuduro", youtubeId:"7zp1TbLFPp8" },
+    { artist:"Wisin & Yandel", title:"Rakata", youtubeId:"Y3tZQK9Yq7g" },
+    { artist:"Ivy Queen", title:"Quiero Bailar", youtubeId:"Vjw92oUduEM" },
+    { artist:"Tego Calderon", title:"Pa Que Retozen", youtubeId:"g2n0hVQFQ0w" },
+    { artist:"Plan B", title:"Fanatica Sensual", youtubeId:"dW2MmuA1nI4" },
+    { artist:"Arcangel", title:"Pa Que La Pases Bien", youtubeId:"KQ6zr6kCPj8" },
+    { artist:"Zion & Lennox", title:"Yo Voy", youtubeId:"U2waT9TxPU0" }
+  ],
+
+  // =========================
+  // SALSA
+  // =========================
+  salsa: [
+    { artist:"Marc Anthony", title:"Vivir Mi Vida", youtubeId:"YXnjy5YlDwk" },
+    { artist:"Hector Lavoe", title:"El Cantante", youtubeId:"Z6fX3xv2bLQ" },
+    { artist:"Grupo Niche", title:"Cali Pachanguero", youtubeId:"b6J1f8z7n0g" },
+    { artist:"Frankie Ruiz", title:"La Cura", youtubeId:"sWzS3Rzv4Qk" },
+    { artist:"Gilberto Santa Rosa", title:"Conteo Regresivo", youtubeId:"mM7J0pK8y9o" }
+  ],
+
+  // =========================
+  // CUMBIA
+  // =========================
+  cumbia: [
+    { artist:"Los Angeles Azules", title:"Como Te Voy A Olvidar", youtubeId:"mR0Kj8vY7hQ" },
+    { artist:"Sonora Dinamita", title:"Mi Cucu", youtubeId:"kYxS4FvZ0dA" },
+    { artist:"Grupo Frontera", title:"No Se Va", youtubeId:"b7g7x8d9kQ0" },
+    { artist:"Bronco", title:"Que No Quede Huella", youtubeId:"mDURChQ7w54" }
+  ],
+
+  // =========================
+  // MERENGUE
+  // =========================
+  merengue: [
+    { artist:"Juan Luis Guerra", title:"La Bilirrubina", youtubeId:"acC2fJ4kY2E" },
+    { artist:"Elvis Crespo", title:"Suavemente", youtubeId:"WPiEbYSF9kE" },
+    { artist:"Wilfrido Vargas", title:"El Africano", youtubeId:"eZk0eH0Jq9Y" }
+  ],
+
+  // =========================
+  // BANDA
+  // =========================
+  banda: [
+    { artist:"Banda MS", title:"El Color de Tus Ojos", youtubeId:"YdXb5zJ1wH4" },
+    { artist:"Julion Alvarez", title:"Te Hubieras Ido Antes", youtubeId:"qQ9xFv6P2tU" },
+    { artist:"Christian Nodal", title:"Adios Amor", youtubeId:"0D5H4YlqkZ0" }
+  ],
+
+  // =========================
+  // CORRIDOS
+  // =========================
+  corridos: [
+    { artist:"Peso Pluma", title:"Ella Baila Sola", youtubeId:"_S3UjMZC0Tk" },
+    { artist:"Natanael Cano", title:"Amor Tumbado", youtubeId:"Z6fX3xv2bLQ" },
+    { artist:"Fuerza Regida", title:"TQM", youtubeId:"lZiaYpD9ZrI" }
+  ],
+
+  // =========================
+  // ROCK EN ESPAÑOL
+  // =========================
+  rockEnEspanol: [
+    { artist:"Soda Stereo", title:"De Musica Ligera", youtubeId:"T_FkEw27XJ0" },
+    { artist:"Mana", title:"Clavado En Un Bar", youtubeId:"b-9vT7eexiU" },
+    { artist:"Heroes del Silencio", title:"Entre Dos Tierras", youtubeId:"SzimletXB7M" }
+  ],
+
+  // =========================
+  // ROCK GUATEMALTECO
+  // =========================
+  rockGuatemalteco: [
+    { artist:"Malacates Trebol Shop", title:"Morena", youtubeId:"SbdkPp-6uK4" },
+    { artist:"Bohemia Suburbana", title:"Peces e Iguanas", youtubeId:"V9Wb9R7u8dQ" },
+    { artist:"Alux Nahual", title:"Como Un Duende", youtubeId:"FQ9c0nGQk9A" }
   ]
 
 };
