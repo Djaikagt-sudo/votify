ea82d2dd7b1276f3a1ffb4ae299e117c092dec4f
Charly Garcia <charlygarcia@MacBook-Pro-de-Charly.local>
2026-03-03 23:46:10 -0600

    Add new songs to library

--- src/data/library.js
+++ src/data/library.js
@@ -25,13 +25,20 @@ export const library = {
   // =========================
   reggaetonAntiguo: [
     { artist:"Daddy Yankee", title:"Gasolina", youtubeId:"BULPP5TIfHY" },
-    { artist:"Don Omar", title:"Danza Kuduro", youtubeId:"DNXIa_KPjTs" },
+    { artist:"Don Omar", title:"Danza Kuduro", youtubeId:"ApqWxdT5HMQ" },
     { artist:"Wisin & Yandel", title:"Rakata", youtubeId:"8zHceVvTtKA" },
     { artist:"Ivy Queen", title:"Yo Quiero Bailar", youtubeId:"BGW-rRorkhk" },
     { artist:"Tego Calderon", title:"Pa Que Retozen", youtubeId:"ldaT-0V10Hw" },
     { artist:"Angel y Khris,", title:"Ven Bailalo", youtubeId:"1g_G6f4_iGM" },
     { artist:"Arcangel", title:"Pa Que La Pases Bien", youtubeId:"FnQeaNBD3Wo" },
-    { artist:"Zion & Lennox, Daddy Yankee ", title:"Yo Voy", youtubeId:"D3-wqAA2uSc" }
+    { artist:"Zion & Lennox, Daddy Yankee ", title:"Yo Voy", youtubeId:"D3-wqAA2uSc" },
+    { artist:"Don Omar", title:"Dile", youtubeId:"nnB21WVjow0" },
+    { artist:"Wisin & Yandel", title:"Pam Pam", youtubeId:"MmlflMxZo-k" },
+    { artist:"Ivy Queen", title:"Yo Quiero Saber", youtubeId:"wEs9z7z8FuQ" },
+    { artist:"Tego Calderon", title:"Punto y Aparte", youtubeId:"GXEDkoX8IxQ" },
+    { artist:"Angel y Khris,", title:"Na de Na", youtubeId:"0VjlNzrWDGw" },
+    { artist:"Arcangel", title:"Me Prefieres A Mi", youtubeId:"ElchRokGp8M" },
+    { artist:"Zion", title:"Zun Da Da", youtubeId:"Ri-PdiYvioY" }
   ],
 
   // =========================
@@ -41,11 +48,27 @@ export const library = {
     { artist:"Marc Anthony", title:"Vivir Mi Vida", youtubeId:"t_CAazLmW_c" },
     { artist:"Hector Lavoe", title:"El Cantante", youtubeId:"xSu3TH3m8P4" },
     { artist:"Grupo Niche", title:"Cali Pachanguero", youtubeId:"SoT_Ls_AH-Y" },
-    { artist:"Frankie Ruiz", title:"La Cura", youtubeId:"AnQdDfnLo54" },
+    { artist:"Frankie Ruiz", title:"La Cura", youtubeId:"wLgiZZyHNJA" },
     { artist:"Gilberto Santa Rosa", title:"Conteo Regresivo", youtubeId:"ZtCPYH4qOks" },
     { artist:"Fresto", title:"Me hace daño verte (Salsa)", youtubeId:"D516C9tjiy8" },
+    { artist:"Grupo Niche", title:"Sin Sent imientos", youtubeId:"9yzx7xrvZQc" },
+    { artist:"Marc Anthony", title:"Valio La Pena", youtubeId:"DtACd6l373w" },
+    { artist:"Maelo Ruiz", title:"Amiga", youtubeId:"8RhFosXuSxA" },
+    { artist:"Maelo Ruiz", title:"Regálame una Noche", youtubeId:"_nFsMI0s-0w" },
+    { artist:"Maelo Ruiz", title:"He Vuelto Por Ti", youtubeId:"jc2StM7WiiQ" },
+    { artist:"Grupo Niche", title:"Una Aventura", youtubeId:"4WYxv09j03U" },
+    { artist:"Grupo Niche", title:"Nuestro Sueño", youtubeId:"qyGx4rxoeSg" },
+    { artist:"Grupo Niche", title:"La Magia De Tus Besos", youtubeId:"sKACMj0irVQ" },
+    { artist:"Frankie Ruiz", title:"Tu Con El", youtubeId:"D76LlPxjlx0" },
+    { artist:"Grupo Niche", title:"Cali Pachanguero", youtubeId:"SoT_Ls_AH-Y" },
+    { artist:"Frankie Ruiz", title:"Deseandote", youtubeId:"P1o22vlvd54" },
+    { artist:"Gilberto Santa Rosa", title:"Que Alguien Me Diga", youtubeId:"mwsjNvMpt6E" },
+    { artist:"Fresto", title:"Un Monton De Estrellas (Salsa)", youtubeId:"Bq2Dpgj30pk" },
+    { artist:"Grupo Niche", title:"Tu Amor Me Hace Bien", youtubeId:"WIG1jwYD67c" },
     { artist:"Oscas D.León", title:"Llorarás", youtubeId:"aO8CuOHgGGY" },
-    { artist:"Maelo Ruiz", title:"Te Va Doler", youtubeId:"6DOTE6o5NOk" }
+    { artist:"Oscas D.León", title:"Detalles", youtubeId:"A95iNmgA9-8" },
+    { artist:"Chichi Peralta", title:"Procura", youtubeId:"YyWfdh7iOuQ" },
+    { artist:"Salsa Kids", title:"Dejame Un Beso Que Me Dure Hasta El Lunes", youtubeId:"YjVr54Pzpi8" }
   ],
 
   // =========================
@@ -82,7 +105,25 @@ export const library = {
   merengue: [
     { artist:"Juan Luis Guerra", title:"La Bilirrubina", youtubeId:"ekoTkEhFI7I" },
     { artist:"Elvis Crespo", title:"Suavemente", youtubeId:"KeeAiDZLi4I" },
-    { artist:"Wilfrido Vargas", title:"El Africano", youtubeId:"1DbXzlhKS5s" }
+    { artist:"Wilfrido Vargas", title:"El Africano", youtubeId:"1DbXzlhKS5s" },
+    { artist:"Elvis Crespo", title:"Nuestra Canción", youtubeId:"hdMD3rP2td8" },
+    { artist:"Elvis Crespo", title:"Tu Sonrisa", youtubeId:"MQkG92XbGpA" },
+    { artist:"Joseph Fonseca", title:"Noches De Fantasia", youtubeId:"XHO9SQLFp14" },
+    { artist:"Joseph Fonseca", title:"Que Levante La Mano", youtubeId:"WmLpIqassOc" },
+    { artist:"Ramón Orlando", title:"Te Compro A Tu Novia", youtubeId:"e09zoT6-G9A" },
+    { artist:"Hermanos Rosario", title:"En El Fin De Semana", youtubeId:"kV0bkeWNf8s" },
+    { artist:"Hermanos Rosario", title:"Video Clip", youtubeId:"zNQ0iMPt3UM" },
+    { artist:"Hermanos Rosario", title:"La Dueña Del Swing", youtubeId:"AVixieFDTh4" },
+    { artist:"Hermanos Rosario", title:"En Estas Navidades", youtubeId:"S8LWhf0XJ1M" },
+    { artist:"Proyecto Uno", title:"El Tiburón", youtubeId:"8ApHOecpxbA" },
+    { artist:"Proyecto Uno", title:"Another Night", youtubeId:"d5ox7D9lt3g" },
+    { artist:"Proyecto Uno", title:"25 Horas Al Dia", youtubeId:"eOQRqtwupL8" },
+    { artist:"La Mákina", title:"Nadie Se Muere", youtubeId:"T2iZjtUsw2U" },
+    { artist:"Oro Solido", title:"Abusadora", youtubeId:"FZ_5MG0vEkc" },
+    { artist:"Oro Solido", title:"El Virao", youtubeId:"CHkbuBPyzIA" },
+    { artist:"Toño rosario", title:"Dale Vieja Dale", youtubeId:"EMTX33MGvaI" },
+    { artist:"Ilegales", title:"La Morena", youtubeId:"mxE5KQJEpnE" },
+    { artist:"Beéle, Elvis Crespo", title:"la patadita", youtubeId:"rs29CX-GZX8" }
   ],
 
   // =========================
@@ -91,15 +132,51 @@ export const library = {
   banda: [
     { artist:"Banda MS", title:"El Color de Tus Ojos", youtubeId:"uHfKXHXjAfs" },
     { artist:"Julion Alvarez", title:"Te Hubieras Ido Antes", youtubeId:"ORddfRSJGFY" },
-    { artist:"Christian Nodal", title:"Adios Amor", youtubeId:"VChgvtLrdPk" }
+    { artist:"Christian Nodal", title:"Adios Amor", youtubeId:"VChgvtLrdPk" },
+    { artist:"Banda MS, Fuerza Regida", title:"Seguro Le Dolio", youtubeId:"cMd4F0f8GOg" },
+    { artist:"Fuerza Regida", title:"Triston", youtubeId:"i8YDQLYwEug" },
+    { artist:"La Arrolladora Banda El Limón", title:"Mi Segunda Vida", youtubeId:"L6GufO2ylvs" },
+    { artist:"La Arrolladora Banda El Limón", title:"El Final De Nuestra Historia", youtubeId:"0JyIK2-oD9c" },
+    { artist:"La Arrolladora Banda El Limón", title:"El Ruido De Tus Zapatos", youtubeId:"CnRznRqNm-s" },
+    { artist:"La Arrolladora Banda El Limón", title:"Ya Es Muy Tarde", youtubeId:"YmQ06FzU7vY" },
+    { artist:"La Arrolladora Banda El Limón", title:"Llamada De Mi Ex", youtubeId:"Nb9WBy1oiE4" },
+    { artist:"La Arrolladora Banda El Limón", title:"Cedí", youtubeId:"KnezK5Kpisc" },
+    { artist:"Banda MS", title:"No Elegí Conocerte", youtubeId:"aYkk1wHgdYQ" },
+    { artist:"Banda MS", title:"Mi Mayor Anhelo", youtubeId:"2lEV9STNW3o" },
+    { artist:"Banda MS", title:"Por Mi No Te Detengas", youtubeId:"ojJJSeB6go" },
+    { artist:"Banda MS", title:"Tengo Que Colgar", youtubeId:"nJTf0Slqljs" },
+    { artist:"Banda MS", title:"Mi Olvido", youtubeId:"Hpdmz4_YSAc" },
+    { artist:"Banda MS", title:"Tu Postura", youtubeId:"UaH3GcWpE60" },
+    { artist:"Julion Alvarez", title:"Rey Sin Reyna", youtubeId:"rrW4dEazmbg" },
+    { artist:"Christian Nodal", title:"De Los Besos Que Te Di", youtubeId:"vH7O3Gh78ns" },
+    { artist:"Grupo Firme", title:"Que Onda Perdida", youtubeId:"duSJsl2uCx8" },
+    { artist:"Grupo Firme", title:"Pídeme", youtubeId:"6hGUeK45GrY" },
+    { artist:"Grupo Firme", title:"El Beneficio De La Duda", youtubeId:"2Rl8Oyj5dIk" },
+    { artist:"Grupo Firme", title:"Ya Superame", youtubeId:"ij1-jhiKa64" },
+    { artist:"Grupo Firme", title:"En Tu Perra Vida", youtubeId:"nI2E2GM2UBY" },
+    { artist:"Grupo Firme", title:"Calidad", youtubeId:"pF7R85jZjeA" },
+    { artist:"Grupo Firme", title:"Hubieramos", youtubeId:"s9TzvvJrwao" },
+    { artist:"Julion Alvarez", title:"Regalo De Dios", youtubeId:"JP_gHLcK84c" },
+    { artist:"Christian Nodal", title:"No Te Contaron Mal", youtubeId:"cI6gATQoSew" },
+    { artist:"La Arrolladora Banda El Limón", title:"Aqui Hay Pa Llevar", youtubeId:"0pgFzYJcIKs" },
+    { artist:"Julion Alvarez", title:"Que Te Vaya Bien", youtubeId:"YanNJSzfIg0" },
+    { artist:"Christian Nodal", title:"Ya No Somos Ni Seremos", youtubeId:"ou9FkfdH4Vc" },
+    { artist:"Banda MS, Alfredo Olivas", title:"Tengo Claro", youtubeId:"Gt3ycBic_tY" },
+    { artist:"Julion Alvarez", title:"Afuera Está Lloviendo", youtubeId:"ytkGFApHa_c" },
+    { artist:"Christian Nodal", title:"Lo Tienes Todo", youtubeId:"VA-M9u4FElM" },
+    { artist:"Banda MS", title:"Cuál Adiós", youtubeId:"HgY21fIPkSk"},
+    { artist:"Julion Alvarez", title:"Cero Empatía", youtubeId:"3rev122nSSo" },
+    { artist:"Grupo Firme, Luis Alfonso Partida", title:"Que Te Quieran Bonito", youtubeId:"FC1TFZvGQRE" },
+    { artist:"Banda MS", title:"Cuál Adiós", youtubeId:"HgY21fIPkSk"},
+    { artist:"Julion Alvarez", title:"Cero Empatía", youtubeId:"3rev122nSSo" },
+    { artist:"Grupo Firme, Luis Alfonso Partida", title:"Que Te Quieran Bonito", youtubeId:"qtlb5xu1XBw" }
+
   ],
 
   // =========================
   // CORRIDOS
   // =========================
   corridos: [
-    // ⚠️ Si quedan vacíos, la TV debe saltarlos sin romperse.
-    // ✅ Pega IDs reales (11 chars) cuando los tengas.
     { artist:"Peso Pluma", title:"Ella Baila Sola", youtubeId:"H3VSYxUu4P0" },
     { artist:"Natanael Cano", title:"Amor Tumbado", youtubeId:"SO3qpwvkRl8" },
     { artist:"Fuerza Regida", title:"TQM", youtubeId:"H8U68cYM7vU" }
